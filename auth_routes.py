7afd3b306c97871a295af8a20021b6036d5cfc63
Rafael Cutrim <rafaelcutrimalmeida@gmail.com>
2025-12-09 15:37:58 -0300

    Adicionada a função de abrir sessões no banco de dados

--- auth_routes.py
+++ auth_routes.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from models import Usuario
+from dependences import pegar_sessao
 
 auth_router = APIRouter(prefix= "/auth", tags= ["auth"])
 
@@ -9,3 +11,15 @@ async def autenticar():
     Essa é a rota padrão de autenticação do sistema
     """
     return{"Mensagem": "Você acessou a rota de autenticação", "Autenticado" : "False"}
+
+@auth_router.post("/criar_conta")
+async def criar_conta(email: str, senha: str,nome : str, session = Depends(pegar_sessao()) ):
+
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+    if usuario:
+        return {"Mensagem": "ja existe"}
+    else:
+        novo_usuario = Usuario(nome,email,senha)
+        session.add(novo_usuario)
+        session.commit()
+        return {"Mensagem":"usuario cadastrado com sucesso"}
